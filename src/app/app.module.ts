9acbe3c5a110404582d0ed24c6bd37c9f0dcd7e9
daniel.barbosa <dmatrixx@gmail.com>
2023-08-05 01:13:47 -0300

    exercicios da atividade 07 - componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterPageComponent } from './components/footer-page/footer-page.component';
 import { HeaderPageComponent } from './components/header-page/header-page.component';
+import { BodyPageComponent } from './components/body-page/body-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterPageComponent,
-    HeaderPageComponent
+    HeaderPageComponent,
+    BodyPageComponent
   ],
   imports: [
     BrowserModule,
