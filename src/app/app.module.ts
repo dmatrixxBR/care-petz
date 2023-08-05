ec976df7a4d19308dec10208237c8ba08d055833
daniel.barbosa <dmatrixx@gmail.com>
2023-08-04 23:14:12 -0300

    gitflow-feature-stash: atividade-07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterPageComponent } from './components/footer-page/footer-page.component';
+import { HeaderPageComponent } from './components/header-page/header-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterPageComponent,
+    HeaderPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/footer-page/footer-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-page',
+  templateUrl: './footer-page.component.html',
+  styleUrls: ['./footer-page.component.css'],
+})
+export class FooterPageComponent {
+}
