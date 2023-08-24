b1d596b0ef799dd32b6738350b9db61eedf80a1e
daniel.barbosa <dmatrixx@gmail.com>
2023-08-24 00:08:58 -0300

    validações e submit button dos forms

--- src/app/models/agenda.ts
+++ src/app/models/agenda.ts
@@ -5,12 +5,18 @@ import { v4 as uuid } from 'uuid';
 export class Agenda {
 
     public codigoAgenda:string = uuid();
-    public dataAgenda : string = new Date().toISOString().substring(0, 10);;
+    public dataAgenda! : string; // new Date().toISOString().substring(0, 10);
     public horaAgenda : string = new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
     public clienteAgenda! : Cliente;
     public servicoAgenda!: Servico;
     public valorServico!: number;
 
-constructor(){}
+constructor(){
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    this.dataAgenda = `${dd}/${mm}/${yyyy}`;
+}
 
 }
