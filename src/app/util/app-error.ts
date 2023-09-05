680ee861c0bd42923b2eb6d1cb2b93e949e932f3
daniel.barbosa <dmatrixx@gmail.com>
2023-09-05 13:45:31 -0300

    Atividade 13

--- src/app/util/app-error.ts
+++ src/app/util/app-error.ts
@@ -1,2 +1,8 @@
-export class AppError {
+import { HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+
+export class AppError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
 }
